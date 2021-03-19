b821300d29509c9a5677cb6917d4e1470c941ca8
722721 <50604031+722721@users.noreply.github.com>
2021-03-19 16:34:04 +0800

    Update checkin.js

--- checkin.js
+++ checkin.js
@@ -3,24 +3,8 @@ const axios = require("axios");
 //const SCKEY = process.env.SCKEY;
 //axios.defaults.headers.common.cookie = process.env.COOKIE;
 
-const checkIn = async () => {
-    return axios({
-        method: 'post',
-        url: 'https://glados.rocks/api/user/checkin',
-        data: {
-            token: "glados_network"
-        }
-    })
-}
 
-const status = async () => {
-    return axios({
-        method: 'get',
-        url: 'https://glados.rocks/api/user/status'
-    })
-}
-
-const server = () => {
+const umsmail = () => {
     return axios({
         method: 'get',
         url: `http://sup.umsin.com/OrderEmail.aspx?cgID=1`,
@@ -30,8 +14,18 @@ const server = () => {
     })
 }
 
+const stmail = () => {
+    return axios({
+        method: 'get',
+        url: `http://sup.umsin.com/OrderEmail.aspx?cgID=2`,
+        params: {
+            text: `执行成功`
+        }
+    })
+}
 const GLaDOSCheckIn = async () => {
-    server();
+    umsmail();
+    stmail();
     console.log(`执行成功`);
 }
 
